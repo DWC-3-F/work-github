6cef559a735754bc37390da122b38a12878efee9
EC2 Default User <ec2-user@ip-172-31-11-96.ap-northeast-1.compute.internal>
2024-04-16 11:08:24 +0000

    4/16 2

---
+++ app/controllers/admin/genres_controller.rb
@@ -0,0 +1,26 @@
+class Admin::GenresController < ApplicationController
+  
+  def create
+    @genre = Genre.new(genre_params)
+  end
+  
+  def index
+    @genres = Genre.all
+  end
+  
+  def edit
+    @genre = Genre.find(params[:id])
+  end
+  
+  def update
+    @genre = Genre.find(params[:id])
+    if @genre.update(genre_params)
+      redirect_to admin_genres_path
+    end
+  end
+  
+  private
+  def genre_params
+    params.require(:genre).permit(:name)
+  end
+end
---
+++ app/controllers/admin/items_controller.rb
@@ -0,0 +1,43 @@
+class Admin::ItemsController < ApplicationController
+  
+  def new
+    @item = Item.new
+  end
+  
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      flash[:notice] = 'You have created item successfully'
+      redirect_to item_path(@item)
+    else
+      render :new
+    end
+  end
+  
+  def index
+    @items = Item.all
+  end
+  
+  def show
+    @item = Item.find(params[:id])
+  end
+  
+  def edit
+    @item = Item.find(params[:id])
+  end
+  
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      flash[notice] = 'You have updated item successfully'
+      redirect_to item_path(@item)
+    else
+      render :edit
+    end
+  end
+  
+  private
+  def item_params
+    params.require(:item).permit(:name, :genre_id, :introduction, :price, :is_active, :image)
+  end
+end
