d009d5d5745855731c72d0f980107ed621c84e20
EC2 Default User <ec2-user@ip-172-31-3-209.ap-northeast-1.compute.internal>
2024-04-17 12:02:58 +0000

    管理者ログイン

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_out_path_for(resource)
-    about_path
+    admin_path
   end
 
   def configure_permitted_parameters
