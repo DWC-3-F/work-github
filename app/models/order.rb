0eb6e1f791120a734290e8eaa9b813af04b6de73
EC2 Default User <ec2-user@ip-172-31-11-96.ap-northeast-1.compute.internal>
2024-04-21 11:50:38 +0000

    4/21 7

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,4 +3,12 @@ class Order < ApplicationRecord
   has_many :order_datails
   
   enum payment_method: { credit_card: 0, transfer: 1 }
+  
+  enum status: {
+    awaiting_payment: 0,
+    payment_confirmed: 1,
+    in_production: 2,
+    preparing_for_shipping: 3,
+    shipped: 4
+  }
 end
--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,3 +1,10 @@
 class OrderDetail < ApplicationRecord
   belongs_to :order
+  
+  enum making_status: {
+    not_available: 0,
+    waiting: 1,
+    in_progress: 2,
+    completed: 3
+  }
 end
