1ab4ebdc703cfdda341447fcb8c998c2eac391f1
EC2 Default User <ec2-user@ip-172-31-11-96.ap-northeast-1.compute.internal>
2024-04-22 03:53:25 +0000

    4/22 2

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -17,7 +17,6 @@ class Public::CartItemsController < ApplicationController
     redirect_to cart_items_path
   end 
 
-
   def destroy_all
     @cart_items = CartItem.all
     @cart_items.destroy
@@ -26,14 +25,14 @@ class Public::CartItemsController < ApplicationController
  
   def create
     @customer = current_customer
-    @cart_item = @customer.cart_items.new(cart_item_params)
+    @cart_item = @customer.CartItem.new(cart_item_params)
     if @customer.cart_items.find_by(item_id: params[:cart_item][:item_id]).present?
       cart_item = @customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
       cart_item.amount += params[:cart_item][:amount].to_i
       cart_item.save
       redirect_to cart_items_path
     elsif @cart_item.save
-    　@cart_items = @customer.cart_items.all
+    　@cart_items = @customer.CartItem.all
     　render 'index'
     else
       render 'index'
