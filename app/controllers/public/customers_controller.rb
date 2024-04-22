a8976c4831ff0b7b7a6e138223d85ee7762cceda
meronpandog <j.meronpan.1101@gmail.com>
2024-04-22 05:01:37 +0000

    kawa

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,14 +1,14 @@
 class Public::CustomersController < ApplicationController
   before_action :authenticate_customer!
-  
+
   def show
     @customer = current_customer
   end
-  
+
   def edit
     @customer = current_customer
   end
-  
+
   def update
     @customer = current_customer
     if @customer.update(customer_params)
@@ -17,11 +17,11 @@ class Public::CustomersController < ApplicationController
       render :edit
     end
   end
-  
+
   def check
     @customer = current_customer
   end
-  
+
   def withdraw
     @customer = current_customer
     @customer.update(is_active: false)
@@ -29,9 +29,9 @@ class Public::CustomersController < ApplicationController
     flash[:notice] = "退会済みです"
     redirect_to root_path
   end
-  
+
   private
-  
+
   def customer_params
     params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :post_code, :address, :telephone_number, :email)
   end
