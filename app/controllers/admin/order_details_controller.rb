3fffe1e9880747c78f9a8760dc7e59a6caef8bfc
EC2 Default User <ec2-user@ip-172-31-11-96.ap-northeast-1.compute.internal>
2024-04-23 09:22:13 +0000

    制作ステータス対応

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -2,9 +2,31 @@ class Admin::OrderDetailsController < ApplicationController
   before_action :authenticate_admin!
   
   def update
-    @order_detail = Order_datail.find(params[:id])
-    if @order_detail.update(:making_status)
-      redirect_to admin_order_path
+    @order_detail = OrderDetail.find(params[:id])
+    @order_detail.update(making_status: params[:order_detail][:making_status])
+    order = @order_detail.order
+    if params[:order_detail][:making_status] == "in_progress"
+       order.update(status:"in_production")
     end
+    if is_all_order_details_making_completed(order)
+      order.update(status: 'preparing_for_shipping')
+    end  
+    flash[:notice] = "更新に成功しました。"
+    redirect_to admin_order_path(@order_detail.order.id)
+  end
+  
+  private
+  
+  def order_detail_params
+    params.require(:order_detail).permit(:making_status)
+  end
+  
+  def is_all_order_details_making_completed(order)
+    order.order_details.each do |order_detail|
+      if order_detail.making_status != 'completed'
+        return false 
+      end
+    end
+    return true
   end
 end
--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -4,4 +4,23 @@ class Admin::OrdersController < ApplicationController
     @order = Order.find(params[:id])
   end
   
+  
+  def update
+    @order = Order.find(params[:id])
+    if @order.update(order_params)
+      flash[:notice] = "注文ステータスの更新に成功しました"
+      redirect_to admin_order_path(@order)
+    else
+      flash[:alert] = "注文ステータスの更新に失敗しました"
+      render :show
+    end
+  end
+  
+  
+  private
+  
+  def order_params
+    params.require(:order).permit(:status)
+  end
+  
 end
